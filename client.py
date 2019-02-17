399a136522a686349231b5299f4014728e0f090c
moosenahmad <45017633+moosenahmad@users.noreply.github.com>
2019-02-17 15:13:33 -0500

    Updated client comm

--- client.py
+++ client.py
@@ -6,8 +6,24 @@ port = 8080
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host,port))
 print("You are connected to the server\n")
+data = s.recv(1024).decode('utf-8')
+print('Server: ' + data)
 while 1:
-     data = s.recv(1024).decode('utf-8')
-     print('Server: ' + data)
      if(data == "cmdclose"):
-          break
+        break
+     elif(data == "clientfile"):
+        s.sendall("ready".encode('utf-8'))
+        f = open('clients.txt','wb')
+        l = s.recv(1024)
+        while(l):
+                if(l == "done...transfer"):
+                     break
+                else:
+                     f.write(l)
+                     l = s.recv(1024)
+        f.close()
+	s.sendall("Done receiving".encode('utf-8'))
+     else:
+	data = s.recv(1024).decode('utf-8')
+	print('Server: ' + data)
+	s.sendall("Received".encode('utf-8'))
